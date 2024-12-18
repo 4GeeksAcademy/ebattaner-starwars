26747ba2d42d370d76a85cb6d60fc8911756f8d9
battanair <battanedd@gmail.com>
2024-12-18 21:05:50 +0000

    hola

--- src/pages/Vehicleunico.jsx
+++ src/pages/Vehicleunico.jsx
@@ -1,5 +1,26 @@
+import { isEmpty } from "lodash";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 const Vehicleunico = () => {
-  return <h1>Hola</h1>;
+  const { id } = useParams();
+  const [vehicleUnico, setVehicleUnico] = useState({});
+  const url = "https://www.swapi.tech/api/vehicles";
+
+  const fetchVehicleUnico = () => {
+    fetch(`${url}/${id}`, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => setVehicleUnico(data.result))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchVehicleUnico();
+  }, []);
+
+  return (
+    <>{!isEmpty(vehicleUnico) && <h1>{vehicleUnico.properties.model}</h1>}</>
+  );
 };
 
 export default Vehicleunico;
